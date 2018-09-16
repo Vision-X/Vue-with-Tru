d8f15afacdf4606331d8e56356f8989fb5813b30
Vision-X <michael.s.marlow@gmail.com>
2018-09-15 18:50:37 -0600

    add fetch functionality and conditionally rendered Shoes component

---
+++ src/components/TheShoes.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <ul>
+      <li v-for="shoe in shoes">
+        <p>{{ shoe.name }}</p>
+        <p>{{ shoe.relDate }}</p>
+      </li>
+    </ul>
+  </section>
+</template>
+
+
+<script>
+export default {
+  name: 'TheShoes',
+  props: {
+    shoes: {
+      type: Array,
+      required: true
+    },
+  }
+};
+</script>
+
+
+<style>
+li {
+  list-style-type: none;
+  padding: 1rem;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="home">
     <TheHeader />
-    <TheHero v-bind:inputText="inputData"/>
+    <TheHero v-bind:inputText="inputData" />
     <input v-on:keyup="_onChange" class="text-input"></input>
+    <div v-if="loaded">
+      <TheShoes v-bind:shoes="shoeData" />
+    </div>
   </div>
 </template>
 
@@ -11,25 +14,49 @@
 
 import TheHeader from '@/components/TheHeader.vue'
 import TheHero from '@/components/TheHero.vue';
+import TheShoes from '@/components/TheShoes.vue';
 export default {
   name: 'home',
   components: {
     TheHeader,
-    TheHero
+    TheHero,
+    TheShoes,
   },
   data() {
     return {
-      inputData: ''
+      inputData: '',
+      shoeData: [],
+      loaded: false
     }
   },
   methods: {
-    _onChange: function() {
+    _onChange() {
       let typedText = document.querySelector('.text-input').value;
       this.inputData = typedText;
     },
-    _fetchShoes: function() {
+    _fetchShoes() {
       console.log("hi, this is the fetch method");
+      let url = "https://webscraper-to-api.firebaseapp.com/output.json"
+      let dataGetter = response => {
+        let arr = [];
+        for (let key in response) {
+          arr.push(response[key])
+        }
+        this.shoeData = arr;
+        this.loaded = true;
+        this._logState();
+      }
+      return fetch(url)
+             .then(response => response.json())
+             .then(dataGetter)
+             .catch(console.error);
+    },
+    _logState() {
+      console.log("hmmm", this.shoeData);
     }
+  },
+  mounted() {
+    this._fetchShoes();
   }
 };
 </script>
